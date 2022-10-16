9bc859578b62216f306c16673402e606d000fa4c
Fouad Bahari <63747680+FouadBahari@users.noreply.github.com>
2022-10-16 10:38:50 +0100

    fix

--- lib/core/utils/app_constants.dart
+++ lib/core/utils/app_constants.dart
@@ -34,7 +34,7 @@ class AppConstants {
                     fontWeight: FontWeightManager.bold,
                   ),
                 ),
-                child: const Text('ok'),
+                child: const Text('Ok'),
               )
             ],
           );
@@ -48,5 +48,80 @@ class AppConstants {
         msg: msg,
         backgroundColor: color ?? AppColors.primary,
         gravity: gravity ?? ToastGravity.BOTTOM);
+  }
+
+  static Widget loadingIndicatorProgressBar({String? data}) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const CircularProgressIndicator(
+            backgroundColor: Colors.orange,
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Text(
+            data ?? "Setting up your account please wait..",
+            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+          )
+        ],
+      ),
+    );
+  }
+
+  static void snackBarNetwork(
+      {String? msg, GlobalKey<ScaffoldState>? scaffoldState}) {
+    ScaffoldMessenger.of(scaffoldState!.currentContext!).showSnackBar(
+      SnackBar(
+        backgroundColor: Colors.red,
+        duration: const Duration(seconds: 3),
+        content: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [Text("$msg"), const Icon(Icons.warning)],
+        ),
+      ),
+    );
+  }
+
+  static void snackBar(
+      {required String msg, required GlobalKey<ScaffoldState> scaffoldState}) {
+    ScaffoldMessenger.of(scaffoldState.currentContext!).showSnackBar(SnackBar(
+      content: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            msg,
+            style: const TextStyle(fontWeight: FontWeight.w500),
+          ),
+          const CircularProgressIndicator(),
+        ],
+      ),
+    ));
+  }
+
+  void push({required BuildContext context, required Widget widget}) {
+    Navigator.push(context, MaterialPageRoute(builder: (_) => widget));
+  }
+
+  void toast(String message) {
+    Fluttertoast.showToast(
+        msg: message,
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0);
+  }
+
+  Widget verticalDivider() {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 4),
+      height: 18,
+      width: 1.0,
+      decoration: BoxDecoration(color: Colors.black.withOpacity(.4)),
+    );
   }
 }
